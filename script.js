284971eb97e624a845677f5e273851eb975c997c
paulosaraiva67 <paulosaraiva67@gmail.com>
2024-10-13 08:32:32 +0100

    --no-edit

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-window.alert("Funcionando");
+// Parte 1 do código
+const numero = document.querySelector(".numero-principal");
+const botaoMenos = document.querySelector(".botao.decremento");
+const botaoMais = document.querySelector(".butao.incremento");
+const botaoReset = document.querySelector(".butao.reset");
+
+// Parte 2 do código
+botaoMais.addEventListener("click", function () {
+    numero.innerHTML = +numero.innerHTML + 1;
+});
+
+botaoMenos.addEventListener("click", function() {
+    numero.innerHTML = +numero.innerHTML - 1;
+});
+
+botaoReset.addEventListener("click", function( ) {
+    numero.innerHTML = 0;
+});
