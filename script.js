427921b2cd7549c26109c6c8f1b071693e1a394a
paulosaraiva67 <paulosaraiva67@gmail.com>
2024-10-13 09:30:29 +0100

    Corrigido o código HTML e JS

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
 // Parte 1 do código
 const numero = document.querySelector(".numero-principal");
 const botaoMenos = document.querySelector(".botao.decremento");
-const botaoMais = document.querySelector(".butao.incremento");
-const botaoReset = document.querySelector(".butao.reset");
+const botaoMais = document.querySelector(".botao.incremento");
+const botaoReset = document.querySelector(".botao.reset");
 
 // Parte 2 do código
 botaoMais.addEventListener("click", function () {
-    numero.innerHTML = +numero.innerHTML + 1;
+	numero.innerHTML = +numero.innerHTML + 1;
 });
-
-botaoMenos.addEventListener("click", function() {
-    numero.innerHTML = +numero.innerHTML - 1;
+botaoMenos.addEventListener("click", function () {
+	numero.innerHTML = +numero.innerHTML - 1;
 });
-
-botaoReset.addEventListener("click", function( ) {
-    numero.innerHTML = 0;
+botaoReset.addEventListener("click", function () {
+	numero.innerHTML = 0;
 });
